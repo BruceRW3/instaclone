d41fab4cc6d859d7f7d3b1fd8620ee2aaf046094
BruceRW3 <brwiggleston@gmail.com>
2022-05-12 15:56:42 -0400

    established database connection

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4200;
 app.use(cors());
 app.use(express.json());
 
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true });
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("Database connection established");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
